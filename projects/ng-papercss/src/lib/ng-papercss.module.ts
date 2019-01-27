d6944026ee006c517d5433e431f59a5d22098363
Daniel Sogl <mytechde@outlook.com>
2019-01-27 16:43:27 +0100

    feat(lib): add basic card component

--- projects/ng-papercss/src/lib/ng-papercss.module.ts
+++ projects/ng-papercss/src/lib/ng-papercss.module.ts
@@ -5,12 +5,13 @@ import { AlertComponent } from './components/alert/alert.component';
 import { ArticleComponent } from './components/article/article.component';
 import { BadgeComponent } from './components/badge/badge.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AlertComponent, ArticleComponent, BadgeComponent, ButtonComponent],
+  declarations: [AlertComponent, ArticleComponent, BadgeComponent, ButtonComponent, CardComponent],
   imports: [
     CommonModule
   ],
-  exports: [AlertComponent, ArticleComponent, BadgeComponent, ButtonComponent]
+  exports: [AlertComponent, ArticleComponent, BadgeComponent, ButtonComponent, CardComponent]
 })
 export class NgPapercssModule { }
--- projects/ng-papercss/src/public_api.ts
+++ projects/ng-papercss/src/public_api.ts
@@ -6,4 +6,5 @@ export * from './lib/components/alert/alert.component';
 export * from './lib/components/article/article.component';
 export * from './lib/components/badge/badge.component';
 export * from './lib/components/button/button.component';
+export * from './lib/components/card/card.component';
 export * from './lib/ng-papercss.module';
