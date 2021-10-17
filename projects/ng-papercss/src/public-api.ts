eba8afcf94ca82031f69906cce41ead493c7df55
Daniel Sogl <me@danielsogl.de>
2021-10-17 10:57:33 +0200

    feat(lib): add bare component modules

--- projects/ng-papercss/src/public-api.ts
+++ projects/ng-papercss/src/public-api.ts
@@ -2,6 +2,16 @@
  * Public API Surface of ng-papercss
  */
 
-export * from './lib/ng-papercss.service';
-export * from './lib/ng-papercss.component';
-export * from './lib/ng-papercss.module';
+export * from './lib/components/alert/alert.module';
+export * from './lib/components/article/article.module';
+export * from './lib/components/badge/badge.module';
+export * from './lib/components/breadcrumb/breadcrumb.module';
+export * from './lib/components/button/button.module';
+export * from './lib/components/card/card.module';
+export * from './lib/components/collapsible/collapsible.module';
+export * from './lib/components/form/form.module';
+export * from './lib/components/modal/modal.module';
+export * from './lib/components/navbar/navbar.module';
+export * from './lib/components/popover/popover.module';
+export * from './lib/components/progress/progress.module';
+export * from './lib/components/tab/tab.module';
