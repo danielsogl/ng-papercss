582aed8746dcc5223086d6d988afa127c59ce39c
Daniel Sogl <me@danielsogl.de>
2021-10-17 12:00:12 +0200

    refactor(lib): delete test components

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CardModule } from 'ng-papercss';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CardModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- projects/ng-papercss/src/public-api.ts
+++ projects/ng-papercss/src/public-api.ts
@@ -1,17 +1,3 @@
 /*
  * Public API Surface of ng-papercss
  */
-
-export * from './lib/components/alert/alert.module';
-export * from './lib/components/article/article.module';
-export * from './lib/components/badge/badge.module';
-export * from './lib/components/breadcrumb/breadcrumb.module';
-export * from './lib/components/button/button.module';
-export * from './lib/components/card/index';
-export * from './lib/components/collapsible/collapsible.module';
-export * from './lib/components/form/form.module';
-export * from './lib/components/modal/modal.module';
-export * from './lib/components/navbar/navbar.module';
-export * from './lib/components/popover/popover.module';
-export * from './lib/components/progress/progress.module';
-export * from './lib/components/tab/tab.module';
